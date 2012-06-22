dc33f554621aae0c60b9c6a0a6f8411125ae4a70
chetan-redkivi <chetan.tomar@redkivi.com>
2012-06-22 16:08:04 +0530

    modified home controller

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,17 +1,12 @@
 class HomeController < ApplicationController
   def index
-#		render :text =>current_user.inspect and return false
 		if !session[:access_token].nil?
 			@graph = Koala::Facebook::API.new(session[:access_token])
 			feed = @graph.get_connections("me", "feed")
 			@friends_profile = @graph.get_connections("me", "friends", "fields"=>"name,birthday,gender")
 			profile = @graph.get_object("me")
 			session[:image]= @graph.get_picture("1310835663",:type=>"large")
-
-#			render :text => @friends_profile.inspect and return false
 			mutualfriends = @graph.get_connections("me", "mutualfriends/630625736")
-#			@graph.put_connections("me", "feed", :message => "Hello ...all!")
-#			@graph.put_wall_post("hey, i'm learning koala")
 		end
 
 		def facefeed
